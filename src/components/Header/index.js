22e9aba405f8b8e1794f4edbfc2d10218f877ab1
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-14 18:07:01 -0300

    Feat Header Component

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import * as S from "./style";
+
+export const Header = () => {
+  return (
+    <S.ContainerHeader>
+      <S.TitleHeader>Looking For Emojis 🔥</S.TitleHeader>
+    </S.ContainerHeader>
+  );
+};
---
+++ src/components/Header/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const ContainerHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 80px;
+
+  background: #121212;
+`;
+
+const TitleHeader = styled.h1`
+  font-size: 24px;
+  color: #e2e2e2;
+`;
+
+export { ContainerHeader, TitleHeader };
