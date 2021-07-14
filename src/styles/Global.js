7018efe03b75f89b33aff2cccfc508b65d531a6f
Ramon Xavier <ramon1234rxm@gmail.com>
2021-07-14 18:05:55 -0300

    Create a Global Style

---
+++ src/styles/Global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+
+
+
+`;
+
+export { GlobalStyle };
